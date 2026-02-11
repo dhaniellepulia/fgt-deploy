ce5b47433be696832f468ef78d9a8fd1fb35d7e2
Dhanielle Jhoshua Pulia <dhaniellepulia@gmail.com>
2026-02-11 17:17:12 +0800

    Regenerate

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,6 +6,11 @@ import { Link, Navigate } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
 import { useNavigate } from "react-router-dom";
 import LanguageSelector from "../components/LanguageSelector";
+import gradient from "../assets/gradient.png";
+import threeD from "../assets/3D asset 1.png";
+import logo from "../assets/logo PNE.png";
+import phoneImg from "../assets/phone number.png";
+import emailImg from "../assets/email.png";
 
 function Login() {
   const { login } = useAuth();
@@ -60,26 +65,18 @@ function Login() {
         <div className="absolute bottom-0 left-0 pointer-events-none">
           <img
             className="max-w-150 lg:max-w-225 h-auto"
-            src="../src/assets/gradient.png"
+            src={gradient}
             alt="gradient"
           />
         </div>
         <div className="w-full lg:w-1/2 flex items-center justify-center px-6">
-          <img
-            className="w-full h-auto"
-            src="../src/assets/3D asset 1.png"
-            alt="signup picture"
-          />
+          <img className="w-full h-auto" src={threeD} alt="signup picture" />
         </div>
         {/* Login form */}
         <div className="w-full lg:w-1/2 flex items-center justify-center px-1">
           <div className="w-full p-8">
             <div className="mb-5 w-full">
-              <img
-                className="w-full h-auto max-w-32"
-                src="../src/assets/logo PNE.png"
-                alt="logo"
-              />
+              <img className="w-full h-auto max-w-32" src={logo} alt="logo" />
             </div>
             <h2 className="text-5xl font-semibold  mb-4">
               {t("login.main_title")}
@@ -140,10 +137,10 @@ function Login() {
 
             <div className="flex justify-between gap-5">
               <div className="w-1/2">
-                <img src="../src/assets/phone number.png" alt="Phone" />
+                <img src={phoneImg} alt="Phone" />
               </div>
               <div className="w-1/2">
-                <img src="../src/assets/email.png" alt="Email" />
+                <img src={emailImg} alt="Email" />
               </div>
             </div>
 
