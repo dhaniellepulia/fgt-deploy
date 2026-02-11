f4fb1f74fbfcab40bad2b9d538f7735c573559d7
Dhanielle Jhoshua Pulia <dhaniellepulia@gmail.com>
2026-02-11 21:43:06 +0800

    update

--- server/src/index.js
+++ server/src/index.js
@@ -27,9 +27,7 @@ const SELF_REGISTER_USER_STATUS_ID = Number(
 );
 const ACTIVE_USER_STATUS_ID = Number(process.env.ACTIVE_USER_STATUS_ID || 1);
 
-app.use(
-  cors({ origin: "https://fgt-deploy-client.vercel.app/", credentials: true }),
-);
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 app.set("json replacer", (key, value) =>
